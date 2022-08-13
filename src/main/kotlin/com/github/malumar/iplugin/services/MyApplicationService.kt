0c4167d2df9af2950f4920d432c5b9a7c466ec21
GitHub Action <action@github.com>
2022-08-13 12:54:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/malumar/iplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/malumar/iplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.malumar.iplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.malumar.iplugin.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/malumar/iplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/malumar/iplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.malumar.iplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
